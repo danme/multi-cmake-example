d16af26aa9850818a74b40029680125bc6348806
Daniel Menke <daniel@menke-online.com>
2015-09-05 22:01:32 +0200

    Added tests.

---
+++ modules/base/tests/core/TestCore.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Daniel Menke on 13.05.15.
+//
+
+#include "BaseCore.h"
+
+#include "iostream"
+
+using namespace std;
+
+int main(void) {
+    BasePrinter p;
+    p.printHello();
+    cout << "Hello World from test!" << endl;
+}
