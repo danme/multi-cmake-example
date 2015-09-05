ffd624fad6b7b22914773bd6aa3512be3862232a
Daniel Menke <daniel@menke-online.com>
2015-09-05 22:38:30 +0200

    Added test for adapter A.

---
+++ modules/base/tests/adapterA/TestAdapterA.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Daniel Menke on 13.05.15.
+//
+
+#include "CoreAdapterA.h"
+
+#include "iostream"
+
+using namespace std;
+
+int main(void) {
+    AdapterAPrinter p;
+    p.printA();
+    cout << "Hello World from test!" << endl;
+}
