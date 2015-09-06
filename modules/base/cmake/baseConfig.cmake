2e1f4fde671826bd7b27677dfe7e6d4218544851
Daniel Menke <daniel@menke-online.com>
2015-09-06 20:45:26 +0200

    Renamed package. Set subprojects output name. Make use of components. Separated subproject from package.

--- modules/base/cmake/baseConfig.cmake
+++ modules/base/cmake/baseConfig.cmake
@@ -1,2 +1,26 @@
-include("${CMAKE_CURRENT_LIST_DIR}/baseTargets.cmake")
+# Package export configuration
 
+###
+# Dependencies
+###
+
+#include(CMakeFindDependencyMacro)
+#find_dependency(<dep> <version>)
+
+###
+# Target includes
+###
+
+# must have includes
+include("${CMAKE_CURRENT_LIST_DIR}/BaseCoreTargets.cmake")
+
+# selectable components
+set(_supported_components A B)
+
+foreach(_comp ${Base_FIND_COMPONENTS})
+  if (NOT ";${_supported_components};" MATCHES _comp)
+    set(Base_FOUND False)
+    set(Base_NOTFOUND_MESSAGE "Specified unsupported component: ${_comp}")
+  endif()
+  include("${CMAKE_CURRENT_LIST_DIR}/Base${_comp}Targets.cmake")
+endforeach()
